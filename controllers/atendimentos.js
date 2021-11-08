543c41b8cb5598a0b2847f1ccd3ca875538041b2
dnpadua <37667481+dnpadua@users.noreply.github.com>
2021-11-08 00:57:12 -0300

    Servidor mysql

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,12 @@
+const Atendimento = require('../models/atendimentos');
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Rota de atendimentos GET'));
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
         res.send('Rota de atendimentos POST')
-    });
+    })
 }
