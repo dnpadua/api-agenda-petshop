1b596c35355fe4b10c6a2b0ca87546a0f5ad67bb
dnpadua <37667481+dnpadua@users.noreply.github.com>
2021-11-14 18:46:35 -0300

    Update atendimentos.js

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -79,7 +79,7 @@ class Atendimento {
             if (erro) {
                 res.status(400).json(erro)
             } else {
-                res.status(200).json(resultados)
+                res.status(200).json({...valores, id})
             }
         })
     }
